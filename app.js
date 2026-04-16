e48ee87f23468019776a887a2f9d3d094bfb34a8
mikkel node <mikkel@node>
2026-04-16 18:33:01 +0000

    Lagt til path

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const routes = require("./router/routes");
+const path = require("path");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes);
 
