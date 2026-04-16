d9d97cb68ae36f5ef4ad484e56cd31ed352ca1b2
mikkel node <mikkel@node>
2026-04-16 18:34:30 +0000

    Byttet til foxController

--- router/routes.js
+++ router/routes.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const foxController = require("../controllers/foxController");
+
+router.get("/", foxController.getHome);
+router.post("/vote", foxController.voteFox);
+
+module.exports = router;
